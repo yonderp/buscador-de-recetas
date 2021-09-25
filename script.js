cfeb46e8d8e4bbb8958e4f86c1efee30da44b985
Yonder Peña <djyonder@gmail.com>
2021-09-25 18:38:59 -0500

    cap 4 de 4 completo final proyecto

--- script.js
+++ script.js
@@ -58,6 +58,21 @@ const search = document.getElementById("search"),
         });
     }
 
+    // fetch random meal from API
+    function getRandomMeal() {
+        //clear meals and heading
+        mealsEl.innerHTML = "";
+        resultHeading.innerHTML = "";
+
+        fetch(`https://www.themealdb.com/api/json/v1/1/random.php`)
+        .then(res => res.json())
+        .then (data => {
+            const meal = data.meals[0];
+
+            addMealToDOM(meal);
+        });
+    }
+
     //add meal to DOM
     function addMealToDOM(meal) {
         const ingredients = [];
@@ -84,7 +99,6 @@ const search = document.getElementById("search"),
                     <p>${meal.strInstructions}</p>
                     <h2>Ingredients</h2>
                     <ul>
-                        "hola a ver"
                         "${ingredients.map(ing => `<li>${ing}</li>`).join('')}"
                     </ul>
                 </div>
@@ -94,6 +108,7 @@ const search = document.getElementById("search"),
 
 // even listeners
 submit.addEventListener("submit", searchMeal);
+random.addEventListener("click", getRandomMeal);
 
 mealsEl.addEventListener("click", e => {
     const mealInfo = e.path.find(item => {
